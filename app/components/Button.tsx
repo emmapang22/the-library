66b9bfd5a0765fdda47af9531c990419b96ab96e
Emma Pang <emmapang3@gmail.com>
2026-06-13 10:48:56 +0200

    added extraClasses prop to customize styling to Button component

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -9,15 +9,25 @@ type ButtonProps = {
   children: ReactNode;
   onClick?: () => void;
   buttonType?: ButtonType;
+  extraClasses?: string;
 };
 
-export const Button = ({ children, onClick, buttonType }: ButtonProps) => {
+export const Button = ({
+  children,
+  onClick,
+  buttonType,
+  extraClasses,
+}: ButtonProps) => {
   let className =
-    "px-6 py-2 rounded transition ease-in-out duration-200 font-medium uppercase hover:cursor-pointer ";
+    "px-8 py-2 rounded transition ease-in-out duration-200 font-medium hover:cursor-pointer ";
+
+  if (extraClasses) {
+    className += extraClasses;
+  }
 
   switch (buttonType) {
     case ButtonType.primary:
-      className += "bg-slate-700 text-white hover:bg-slate-800";
+      className += " bg-slate-700 text-white hover:bg-slate-800";
       break;
 
     case ButtonType.secondary:
