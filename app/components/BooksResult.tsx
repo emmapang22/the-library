d2bdc6505f32efece5c13cbf59d7482c2f1f35c3
Emma Pang <emmapang3@gmail.com>
2026-06-13 11:07:10 +0200

    added BooksResult component to fetch and display book search results if there is a query

---
+++ app/components/BooksResult.tsx
@@ -0,0 +1,76 @@
+import { OpenLibraryResponse } from "../models/OpenLibraryResponse";
+import { Button, ButtonType } from "./Button";
+import { BookCover } from "./BookCover";
+
+type BooksResultProps = {
+  query: string;
+};
+
+export const BooksResult = async ({ query }: BooksResultProps) => {
+  if (query) {
+    const response = await fetch(
+      `https://openlibrary.org/search.json?q=${query}&limit=10`,
+    );
+    const data: OpenLibraryResponse = await response.json();
+
+    console.log(data);
+
+    if (!data.docs || data.docs.length === 0) {
+      return <p>Could not find any books matching your search: "{query}"</p>;
+    }
+
+    return (
+      <div className="flex flex-col gap-4 w-full max-w-125 mt-8">
+        {data.docs.map((b) => (
+          <div key={b.key} className="flex p-4 border gap-4">
+            <BookCover
+              cover_edition_key={b.cover_edition_key}
+              title={b.title}
+              author_name={b.author_name}
+            />
+
+            <div className="flex flex-2 flex-col w-full max-w-3xl justify-between">
+              <div className="flex flex-col">
+                {b.series_name && (
+                  <p className="font-bold text-sm">
+                    {b.series_name.join(", ")}{" "}
+                    {!b.series_position || b.series_position[0] === "" ? (
+                      ""
+                    ) : (
+                      <>#{b.series_position?.join(", ")}</>
+                    )}
+                  </p>
+                )}
+
+                <h3 className="font-bold md:text-xl">{b.title}</h3>
+
+                <div className="flex flex-col gap-1">
+                  <p className="text-sm">
+                    By {b.author_name?.join(", ") || "Unknown author"}
+                  </p>
+
+                  {b.first_publish_year ? (
+                    <p className="text-sm">Published: {b.first_publish_year}</p>
+                  ) : (
+                    <p className="text-sm">Published: Unknown</p>
+                  )}
+                </div>
+              </div>
+
+              <div className="mt-4 lg:self-end">
+                <Button
+                  buttonType={ButtonType.primary}
+                  extraClasses={" w-full lg:w-auto "}
+                >
+                  Want to read
+                </Button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  return null;
+};
