357af229e978ae31da03f036e9be8495bdf880da
Emma Pang <emmapang3@gmail.com>
2026-06-14 14:26:02 +0200

    updated Book type to make some properties optional, and added BookModel schema

---
+++ app/models/BookModel.ts
@@ -0,0 +1,16 @@
+import { model, models, Schema } from "mongoose";
+
+const bookSchema = new Schema(
+  {
+    key: { type: String, required: true, unique: true },
+    title: { type: String, required: true },
+    author_name: [String],
+    cover_edition_key: String,
+    first_publish_year: Number,
+    series_name: [String],
+    series_position: [String],
+  },
+  { timestamps: true },
+);
+
+export const BookModel = models.book || model("book", bookSchema);
