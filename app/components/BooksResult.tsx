bc08647dba2dcd14f62f5aea1b48e2b5cbcd83e1
Emma Pang <emmapang3@gmail.com>
2026-06-14 19:08:23 +0200

    refactored BooksResult component to use BookCard for rendering book details

--- app/components/BooksResult.tsx
+++ app/components/BooksResult.tsx
@@ -1,6 +1,6 @@
+import { addToReadingList } from "../actions/bookActions";
 import { OpenLibraryResponse } from "../models/OpenLibraryResponse";
-import { BookCover } from "./BookCover";
-import { WantToReadBtn } from "./WantToReadBtn";
+import { BookCard } from "./bookCard/BookCard";
 
 type BooksResultProps = {
   query: string;
@@ -31,45 +31,12 @@ export const BooksResult = async ({ query }: BooksResultProps) => {
       <h1 className="h4 text-center">Search results for "{query}"</h1>
 
       {data.docs.map((b) => (
-        <div
+        <BookCard
           key={b.key}
-          className="flex p-4 bg-[#f7f7f7] border-2 border-[#b4b4b4] dark:border-[#6e6e6e] rounded gap-4 dark:bg-[#242424]"
-        >
-          <BookCover
-            cover_edition_key={b.cover_edition_key}
-            title={b.title}
-            author_name={b.author_name}
-          />
-
-          <div className="flex flex-2 flex-col w-full justify-between">
-            <div>
-              {b.series_name && (
-                <p className="font-bold text-sm">
-                  {b.series_name.join(", ")}{" "}
-                  {!b.series_position || b.series_position[0] === "" ? (
-                    ""
-                  ) : (
-                    <>#{b.series_position?.join(", ")}</>
-                  )}
-                </p>
-              )}
-
-              <h3 className="font-bold text-[16px] md:text-xl">{b.title}</h3>
-
-              <p className="text-sm">
-                By {b.author_name?.join(", ") || "Unknown author"}
-              </p>
-
-              {b.first_publish_year ? (
-                <p className="text-sm">Published: {b.first_publish_year}</p>
-              ) : (
-                <p className="text-sm">Published: Unknown</p>
-              )}
-            </div>
-
-            <WantToReadBtn book={b} />
-          </div>
-        </div>
+          b={b}
+          buttonText="Want to read"
+          buttonFunction={addToReadingList}
+        />
       ))}
     </div>
   );
