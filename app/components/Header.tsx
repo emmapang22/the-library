a7f3e192125cf6eaa5e9d4b26eddb8a72e8d2fde
Emma Pang <emmapang3@gmail.com>
2026-06-18 22:03:51 +0200

    refactored Header component layout and updated secondary color variable in globals.css

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="font-sans flex justify-center items-center w-full sticky top-0 z-40 pt-4 px-4">
-      <div className="flex justify-between items-center bg-primary text-white dark:bg-primary-lighter w-full lg:w-9/12 px-6 lg:px-8 py-4 rounded-xl">
-        <h2 className="text-xl font-bold">
+    <header className="font-sans flex justify-center items-center w-full sticky top-0 z-40 p-0 md:pt-4 md:px-4">
+      <div className="flex flex-col md:flex-row gap-2 justify-between items-center bg-primary text-white dark:bg-primary-lighter w-full lg:w-9/12 px-6 lg:px-8 py-4 md:rounded-xl shadow-2xl/30 drop-shadow-xl/20">
+        <h2 className="text-xl font-bold hover:text-secondary-lighter">
           <Link href={"/"}>BookCollection</Link>
         </h2>
 
-        <nav>
-          <ul className="flex gap-6 font-medium">
-            <li>
+        <nav className="">
+          <ul className="flex gap-6 font-medium text-center">
+            <li className="hover:text-secondary-lighter">
               <Link href={"/books"}>Search books</Link>
             </li>
-            <li>
+            <li className="hover:text-secondary-lighter">
               <Link href={"/reading-list"}>Reading List</Link>
             </li>
           </ul>
