1c960a3ec954c3930ba4a12461937ff86b4036fd
Emma Pang <emmapang3@gmail.com>
2026-06-18 23:07:32 +0200

    added home page, updated shadows in Header and updated font sizes

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export const Header = () => {
   return (
     <header className="font-sans flex justify-center items-center w-full sticky top-0 z-40 p-0 md:pt-4 md:px-4">
-      <div className="flex flex-col md:flex-row gap-2 justify-between items-center bg-primary text-white dark:bg-primary-lighter w-full lg:w-9/12 px-6 lg:px-8 py-4 md:rounded-xl shadow-2xl/30 drop-shadow-xl/20">
+      <div className="flex flex-col md:flex-row gap-2 justify-between items-center bg-primary text-white dark:bg-primary-lighter w-full lg:w-9/12 px-6 lg:px-8 py-4 md:rounded-xl shadow-xl md:shadow-2xl/30 drop-shadow-lg md:drop-shadow-xl">
         <h2 className="text-xl font-bold hover:text-secondary-lighter">
           <Link href={"/"}>BookCollection</Link>
         </h2>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,33 @@
+import { BookSearch, FolderBookmark } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
-  return;
+  return (
+    <div className="flex flex-col items-center text-center gap-4 w-full md:px-4">
+      <h1>Welcome to BookCollection!</h1>
+      <p>Search books from OpenLibrary and build your own reading list</p>
+
+      <div className="flex flex-col md:flex-row gap-6 items-center justify-center w-full mt-8">
+        <Link
+          href={"/books"}
+          className="bg-primary dark:bg-primary-lighter hover:bg-secondary-darker text-white hover:text-black w-full md:w-auto flex items-center justify-center p-8 rounded-xl"
+        >
+          <div className="flex justify-center items-center gap-4">
+            <BookSearch size={30} />
+            <h3>Search books</h3>
+          </div>
+        </Link>
+
+        <Link
+          href={"/reading-list"}
+          className="bg-primary dark:bg-primary-lighter hover:bg-secondary-darker text-white hover:text-black w-full md:w-auto flex items-center justify-center p-8 rounded-xl"
+        >
+          <div className="flex justify-center items-center gap-4">
+            <FolderBookmark size={30} />
+            <h3>Reading list</h3>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
